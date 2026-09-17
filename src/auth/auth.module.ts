81369b13b2c2eccaf0ec9e2cc6b8a50cc7ef0132
Anish Bag <anishbag18@gmail.com>
2026-09-17 13:16:29 +0530

    guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity.js';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy.js';
 
 
 @Module({
@@ -27,6 +28,6 @@ import { JwtModule } from '@nestjs/jwt';
 ],
 
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
